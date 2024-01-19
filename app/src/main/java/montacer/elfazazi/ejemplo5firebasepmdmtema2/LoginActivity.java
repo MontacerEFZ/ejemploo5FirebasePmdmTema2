6e163d1a81095eae483c3ab42f0077a7bd152b21
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2024-01-19 10:22:54 +0100

    good

--- app/src/main/java/montacer/elfazazi/ejemplo5firebasepmdmtema2/LoginActivity.java
+++ app/src/main/java/montacer/elfazazi/ejemplo5firebasepmdmtema2/LoginActivity.java
@@ -1,14 +1,102 @@
 package montacer.elfazazi.ejemplo5firebasepmdmtema2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import montacer.elfazazi.ejemplo5firebasepmdmtema2.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        binding.btnRegistrarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.txtEmailLogin.getText().toString();
+                String password = binding.txtPasswordLogin.getText().toString();
+
+                if(!email.isEmpty() && !password.isEmpty()){
+                    registrar(email, password);
+                }
+            }
+        });
+
+        binding.btnAccederLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.txtEmailLogin.getText().toString();
+                String password = binding.txtPasswordLogin.getText().toString();
+
+                if(!email.isEmpty() && !password.isEmpty()){
+                    acceder(email, password);
+                }
+            }
+        });
+    }
+
+    private void acceder(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            user = auth.getCurrentUser();
+                            pasarPantalla(user);
+                        }
+                    }
+                })
+                .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void pasarPantalla(FirebaseUser user) {
+        if (user!= null){
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void registrar(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            user = auth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "usuario registrado", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
